5a0d7c409e874a33bf7034140a6271a02ec69b1c
TheIGL1 <163698155+TheIGL1@users.noreply.github.com>
2024-05-09 18:47:09 +0600

    Update main.php

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>API Request Demo</title>
+<title>Resutl API Request </title>
 <style>
   /* Simple CSS for styling */
   body {
